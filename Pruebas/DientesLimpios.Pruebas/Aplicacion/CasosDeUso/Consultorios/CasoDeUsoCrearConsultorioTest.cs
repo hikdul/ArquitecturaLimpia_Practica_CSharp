1a80e2da731bc7a9e903a8f11ddc7fc8a6cc6096
hikhul <hikdul.lio@gmail.com>
2025-09-06 06:41:50 -0400

    UP[CasoUsoCommandCrearConsultorio | TEST]: - se completan las pruebas para este caso de uso - se deja operativo el sistema para su uso

--- Pruebas/DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoCrearConsultorioTest.cs
+++ Pruebas/DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoCrearConsultorioTest.cs
@@ -2,20 +2,24 @@ using System;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Command.CrearConsultorio;
 using DientesLimpios.Aplicacion.Contratos.Persistencia;
 using DientesLimpios.Aplicacion.Contratos.Repository;
+using DientesLimpios.Aplicacion.Excepcion;
 using DientesLimpios.Dominio.Entidades;
 using FluentValidation;
 using FluentValidation.Results;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 
 namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
 {
     [TestClass]
     public class CasoDeUsoCrearConsultorioTest
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         private IRepositoryConsultorios repository;
         private IValidator<CommandCrearConsultorio> validador;
         private IUnidadDeTrabajo unidadDeTrabajo;
         private CasoDeUsoCrearConsultorio casoDeUso;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
         [TestInitialize]
         public void Setup()
@@ -42,6 +46,20 @@ namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
             await repository.Received(1).Agregar(Arg.Any<Consultorio>());
             await unidadDeTrabajo.Received(1).Persistir();
             Assert.AreNotEqual(Guid.Empty, result);
+        }
+
+        [TestMethod]
+        public async Task Handle_cuandoHayError_CallBack()
+        {
+            var command = new CommandCrearConsultorio { Nombre = "consult A" };
+            repository.Agregar(Arg.Any<Consultorio>()).Throws<Exception>();
+            validador.ValidateAsync(command).Returns(new ValidationResult());
+            await Assert.ThrowsExceptionAsync<Exception>(
+                async () => await casoDeUso.Handle(command)
+            );
+
+            await unidadDeTrabajo.DidNotReceive().Persistir();
+            await unidadDeTrabajo.Received(1).Reversar();
         }
     }
 }
