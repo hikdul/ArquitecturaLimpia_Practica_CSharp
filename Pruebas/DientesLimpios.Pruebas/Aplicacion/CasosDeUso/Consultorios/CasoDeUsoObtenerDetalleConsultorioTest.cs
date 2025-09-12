7d75fd84f9b4d85ec3b30694aa07f9197f8c3de2
hikdul <hikdul.lio@gmail.com>
2025-09-12 12:07:56 -0300

    Up[Test]: - se generan pruebas para casoDeUsoObtenerDetalleConsultorio

---
+++ Pruebas/DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoObtenerDetalleConsultorioTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Command.CrearConsultorio;
+using DientesLimpios.Aplicacion.Consultas.ObtenerDetallesConsultorio;
+//using DientesLimpios.Aplicacion.Contratos.Persistencia;
+using DientesLimpios.Aplicacion.Contratos.Repository;
+using DientesLimpios.Aplicacion.Excepcion;
+using DientesLimpios.Dominio.Entidades;
+using FluentValidation;
+using FluentValidation.Results;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using NSubstitute.ExceptionExtensions;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    [TestClass]
+    public class CasoDeUsoObtenerDetalleConsultorioTest
+    {
+        private IRepositoryConsultorios repository;
+        private CasoDeUsoObtenerDetalleConsultorio casoDeUso;
+
+        [TestInitialize]
+        public void Setud()
+        {
+            repository = Substitute.For<IRepositoryConsultorios>();
+            casoDeUso = new CasoDeUsoObtenerDetalleConsultorio(repository);
+        }
+
+        [TestMethod]
+        public async Task Handle_cosultorioExiste_RetornaDTO()
+        {
+            //~: Preparacion
+            string nombreConsultorio = "Consultorio A";
+            var consultorio = new Consultorio(nombreConsultorio);
+            var id = consultorio.Id;
+            var consulta = new ConsultaObtenerDetalleConsultorio { Id = id };
+
+            repository.ObtenerPorId(id).Returns(consultorio);
+
+            //~: Prueba
+            var result = await casoDeUso.Handle(consulta);
+
+            //~: Verificacion
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
+            Assert.AreEqual(nombreConsultorio, result.Nombre);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionNoEncontrado))]
+        public async Task Handle_consultorioNoExiste_lanzaExcepccionNoEncontrado()
+        {
+            var id = Guid.NewGuid();
+            var consulta = new ConsultaObtenerDetalleConsultorio { Id = id };
+
+            repository.ObtenerPorId(id).ReturnsNull();
+
+            await casoDeUso.Handle(consulta);
+        }
+    }
+}
